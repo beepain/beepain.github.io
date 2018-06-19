9aeb66d659acac75f4f0cb2bced42fd6a2a63246
beepain <beepain1@gmail.com>
2018-06-19 04:23:33 +0300

    beepaincommit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+$(function () {
+    setTimeout(function () {
+        $('body').addClass('loaded');
+    }, 300);
+
+    // smooth scroll to #
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+
+    //    wow js detect when block is visible
+    var wow = new WOW(
+        {
+            animateClass: 'animated',
+            offset: 100,
+            callback: function (box) {
+                console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+            }
+        }
+    );
+    wow.init();
+
+
+});
